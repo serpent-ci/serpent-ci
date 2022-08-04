4739693bb3c039e6385d1fa204a588fce300fb39
Simon Bourne <simonbourne@gmail.com>
2022-08-04 12:51:50 +0100

    Add a stub parser

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -1,8 +1,73 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, alphanumeric1, multispace0},
+    combinator::recognize,
+    multi::{many0, many0_count},
+    sequence::{pair, tuple, delimited},
+    IResult, error::ParseError,
+};
+
+#[derive(PartialEq, Debug)]
+pub struct Module {
+    functions: Vec<Function>,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Function {
+    name: String,
+}
+
+pub fn module(input: &str) -> IResult<&str, Module> {
+    let (input, functions) = many0(function)(input)?;
+
+    Ok((input, Module { functions }))
+}
+
+fn function(input: &str) -> IResult<&str, Function> {
+    let (input, (_def, name, _params, _colon, _pass)) = tuple((tag("def"), ws(identifier), ws(tag("()")), ws(tag(":")), tag("pass")))(input)?;
+
+    Ok((
+        input,
+        Function {
+            name: name.to_owned(),
+        },
+    ))
+}
+
+fn identifier(input: &str) -> IResult<&str, &str> {
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0_count(alt((alphanumeric1, tag("_")))),
+    ))(input)
+}
+
+fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+  where
+  F: Fn(&'a str) -> IResult<&'a str, O, E>,
+{
+  delimited(
+    multispace0,
+    inner,
+    multispace0
+  )
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::{module, Module, Function};
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn basic_parsing() {
+        let input = include_str!("../../../tests/parse.py");
+        let (_, module) = module(input).unwrap();
+        assert_eq!(
+            module,
+            Module {
+                functions: vec![Function {
+                    name: "test".to_owned()
+                }]
+            }
+        );
     }
 }
