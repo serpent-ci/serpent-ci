063c8ae18f475e98341ea6416bf5c463432a12cd
Simon Bourne <simonbourne@gmail.com>
2022-08-04 14:58:54 +0100

    Friendlier errors

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -2,12 +2,19 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::{alpha1, alphanumeric1, multispace0},
-    combinator::recognize,
-    error::ParseError,
+    combinator::{all_consuming, recognize},
+    error::ErrorKind,
     multi::{many0, many0_count},
     sequence::{delimited, pair, tuple},
-    IResult,
+    Finish, IResult,
 };
+use nom_greedyerror::{convert_error, GreedyError};
+use nom_locate::LocatedSpan;
+use thiserror::Error;
+
+type Span<'a> = LocatedSpan<&'a str>;
+
+type ParseResult<'a, T> = IResult<Span<'a>, T, GreedyError<Span<'a>, ErrorKind>>;
 
 #[derive(PartialEq, Debug)]
 pub struct Module {
@@ -19,13 +26,32 @@ pub struct Function {
     name: String,
 }
 
-pub fn module(input: &str) -> IResult<&str, Module> {
+#[derive(Error, Debug)]
+#[error("Parse error:\n{0}")]
+pub struct ParseError(String);
+
+impl ParseError {
+    pub fn text(&self) -> &str {
+        &self.0
+    }
+}
+
+pub fn parse(input: &str) -> Result<Module, ParseError> {
+    let (_, script) = match all_consuming(ws(module))(Span::new(input)).finish() {
+        Ok(ok) => Ok(ok),
+        Err(e) => Err(ParseError(convert_error(input, e))),
+    }?;
+
+    Ok(script)
+}
+
+fn module(input: Span) -> ParseResult<Module> {
     let (input, functions) = many0(function)(input)?;
 
     Ok((input, Module { functions }))
 }
 
-fn function(input: &str) -> IResult<&str, Function> {
+fn function(input: Span) -> ParseResult<Function> {
     let (input, (_def, name, _params, _colon, _pass)) = tuple((
         tag("def"),
         ws(identifier),
@@ -37,35 +63,33 @@ fn function(input: &str) -> IResult<&str, Function> {
     Ok((
         input,
         Function {
-            name: name.to_owned(),
+            name: (*name.fragment()).to_owned(),
         },
     ))
 }
 
-fn identifier(input: &str) -> IResult<&str, &str> {
+fn identifier(input: Span) -> ParseResult<Span> {
     recognize(pair(
         alt((alpha1, tag("_"))),
         many0_count(alt((alphanumeric1, tag("_")))),
     ))(input)
 }
 
-fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
-    inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+fn ws<'a, F: 'a, O>(inner: F) -> impl FnMut(Span<'a>) -> ParseResult<'a, O>
 where
-    F: Fn(&'a str) -> IResult<&'a str, O, E>,
+    F: FnMut(Span<'a>) -> ParseResult<'a, O>,
 {
     delimited(multispace0, inner, multispace0)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{module, Function, Module};
+    use crate::{parse, Function, Module};
 
     #[test]
     fn basic_parsing() {
         let input = include_str!("../../../tests/parse.py");
-        let (_, module) = module(input).unwrap();
+        let module = parse(input).unwrap();
         assert_eq!(
             module,
             Module {
