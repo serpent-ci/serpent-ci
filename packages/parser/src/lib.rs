5b441ebd22d3611eaabbcb3f3d159e3b838f5e6f
Simon Bourne <simonbourne@gmail.com>
2022-09-21 13:22:53 +0100

    Parses multiline function bodies

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -1,16 +1,23 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{alpha1, alphanumeric1, multispace0, space0, space1},
+    character::complete::{alpha1, alphanumeric1, line_ending, multispace0, space0, space1},
     combinator::{all_consuming, eof, map, recognize},
     error::{context, ErrorKind},
-    multi::{many0, many_till},
+    multi::{many0, many_till, separated_list1},
     sequence::{delimited, pair, tuple},
     Finish, IResult,
 };
 use nom_greedyerror::{convert_error, GreedyError};
 use nom_locate::LocatedSpan;
 use thiserror::Error;
+
+pub fn parse(input: &str) -> Result<Module, ParseError> {
+    match all_consuming(Module::parse)(Span::new(input)).finish() {
+        Ok((_, module)) => Ok(module),
+        Err(e) => Err(ParseError(convert_error(input, e))),
+    }
+}
 
 type Span<'a> = LocatedSpan<&'a str>;
 
@@ -21,9 +28,69 @@ pub struct Module {
     functions: Vec<Function>,
 }
 
+impl Module {
+    fn parse(input: Span) -> ParseResult<Self> {
+        let (input, (functions, _)) =
+            context("module", many_till(multiline_ws(Function::parse), eof))(input)?;
+
+        Ok((input, Module { functions }))
+    }
+}
+
 #[derive(PartialEq, Eq, Debug)]
 pub struct Function {
     name: String,
+    body: Vec<Statement>,
+}
+
+impl Function {
+    fn parse(input: Span) -> ParseResult<Self> {
+        let (input, (_def, _, name, _params, _colon, body)) = context(
+            "function",
+            tuple((
+                def,
+                space1,
+                identifier,
+                ws(tag("()")),
+                colon,
+                alt((Self::inline_body, Self::block_body)),
+            )),
+        )(input)?;
+
+        Ok((
+            input,
+            Function {
+                name: (*name.fragment()).to_owned(),
+                body,
+            },
+        ))
+    }
+
+    fn inline_body(input: Span) -> ParseResult<Vec<Statement>> {
+        let (input, statement) = context("inline body", Statement::parse)(input)?;
+
+        Ok((input, vec![statement]))
+    }
+
+    fn block_body(input: Span) -> ParseResult<Vec<Statement>> {
+        let (input, _) = discard(pair(space0, line_ending))(input)?;
+        let (input, prefix) = space1(input)?;
+
+        separated_list1(pair(line_ending, tag(*prefix.fragment())), Statement::parse)(input)
+    }
+}
+
+#[derive(Eq, PartialEq, Debug)]
+pub enum Statement {
+    Pass,
+}
+
+impl Statement {
+    fn parse(input: Span) -> ParseResult<Self> {
+        let (input, _pass) = context("statement", pass)(input)?;
+
+        Ok((input, Statement::Pass))
+    }
 }
 
 #[derive(Error, Debug)]
@@ -34,33 +101,6 @@ impl ParseError {
     pub fn text(&self) -> &str {
         &self.0
     }
-}
-
-pub fn parse(input: &str) -> Result<Module, ParseError> {
-    match all_consuming(module)(Span::new(input)).finish() {
-        Ok((_, module)) => Ok(module),
-        Err(e) => Err(ParseError(convert_error(input, e))),
-    }
-}
-
-fn module(input: Span) -> ParseResult<Module> {
-    let (input, (functions, _)) = context("module", many_till(multiline_ws(function), eof))(input)?;
-
-    Ok((input, Module { functions }))
-}
-
-fn function(input: Span) -> ParseResult<Function> {
-    let (input, (_def, _, name, _params, _colon, _pass)) = context(
-        "function",
-        tuple((def, space1, identifier, ws(tag("()")), colon, pass)),
-    )(input)?;
-
-    Ok((
-        input,
-        Function {
-            name: (*name.fragment()).to_owned(),
-        },
-    ))
 }
 
 fn identifier(input: Span) -> ParseResult<Span> {
@@ -120,7 +160,7 @@ operators!((colon, ":"));
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse, Function, Module};
+    use crate::{parse, Function, Module, Statement};
 
     #[test]
     fn basic_parsing() {
@@ -130,7 +170,8 @@ mod tests {
             module,
             Module {
                 functions: vec![Function {
-                    name: "test".to_owned()
+                    name: "test".to_owned(),
+                    body: vec![Statement::Pass],
                 }]
             }
         );
--- tests/parse.py
+++ tests/parse.py
@@ -1,1 +1,2 @@
-def test(): pass
+def test():
+    pass
