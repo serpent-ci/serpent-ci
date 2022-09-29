ad44217dd37039692a7a3967f66fd6d2d462c22b
Simon Bourne <simonbourne@gmail.com>
2022-09-29 18:13:15 +0100

    Start generating HTML from WASM

--- packages/ui/src/main.rs
+++ packages/ui/src/main.rs
@@ -1,1 +1,68 @@
-fn main() {}
+use silkenweb::{
+    elements::{
+        html::{a, button, div, i, li, p, ul, Div, DivBuilder, LiBuilder},
+        svg::svg,
+    },
+    mount,
+    node::element::ElementBuilder,
+    prelude::{HtmlElement, ParentBuilder},
+};
+
+mod css {
+    use silkenweb::css_classes;
+
+    css_classes!(visibility: pub, path: "bootstrap.min.css");
+}
+
+fn button_group() -> DivBuilder {
+    div().class([css::BTN_GROUP]).attribute("role", "group")
+}
+
+fn dropdown_item(name: &str) -> LiBuilder {
+    li().child(a().class([css::DROPDOWN_ITEM]).href("#").text(name))
+}
+
+fn function(name: &str, icon: &str) -> DivBuilder {
+    // TODO: Add `aria` stuff to silkenweb so we don't need to use `attribute`
+    // TODO: `role("group")`
+    // TODO: Dropdown id
+    div().child(
+        button_group()
+            .attribute("aria-label", format!("Function {name}"))
+            .child(
+                button_group()
+                    .child(
+                        button()
+                            .class([css::BTN, css::BTN_OUTLINE_SECONDARY, css::DROPDOWN_TOGGLE])
+                            .id("TODO")
+                            .attribute("data-bs-toggle", "dropdown")
+                            .r#type("button")
+                            .attribute("aria-expanded", "false")
+                            .text(name),
+                    )
+                    .child(
+                        ul().class([css::DROPDOWN_MENU])
+                            .attribute("aria-labelledby", "TODO")
+                            .children([dropdown_item("Run"), dropdown_item("Pause")]),
+                    ),
+            )
+            .child(
+                button()
+                    .r#type("button")
+                    .class([css::BTN, css::BTN_OUTLINE_SECONDARY])
+                    .child(i().class([icon])),
+            ),
+    )
+}
+
+fn collapsed_function(name: &str) -> Div {
+    function(name, "bi-zoom-in").build()
+}
+
+fn expanded_function(name: &str) -> DivBuilder {
+    function(name, "bi-zoom-out").child(svg())
+}
+
+fn main() {
+    mount("app", expanded_function("main_function"));
+}
