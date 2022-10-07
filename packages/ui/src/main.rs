45305a114bf2e7430b44832d65ae3897ec416cf8
Simon Bourne <simonbourne@gmail.com>
2022-10-07 13:22:44 +0100

    Add end element

--- packages/ui/src/main.rs
+++ packages/ui/src/main.rs
@@ -20,6 +20,25 @@ mod css {
     css_classes!(visibility: pub, path: "serpent-ci.scss");
 }
 
+fn dropdown(name: &str) -> Element {
+    button_group()
+        .child(
+            button()
+                .class([bs::BTN, bs::BTN_SECONDARY, bs::DROPDOWN_TOGGLE])
+                .id("TODO")
+                .attribute("data-bs-toggle", "dropdown")
+                .r#type("button")
+                .aria_expanded("false")
+                .text(name),
+        )
+        .child(
+            ul().class([bs::DROPDOWN_MENU])
+                .aria_labelledby("TODO")
+                .children([dropdown_item("Run"), dropdown_item("Pause")]),
+        )
+        .into()
+}
+
 fn button_group() -> DivBuilder {
     div().class([bs::BTN_GROUP]).role("group")
 }
@@ -36,27 +55,11 @@ fn column<'a>(classes: impl IntoIterator<Item = &'a str>) -> DivBuilder {
     div().class(classes.into_iter().chain([bs::D_FLEX, bs::FLEX_COLUMN]))
 }
 
-fn function(name: &str, icon: &str, is_last: bool) -> Element {
+fn function(name: &str, icon: &str) -> Element {
     // TODO: Dropdown id
     let function = button_group()
         .aria_label(format!("Function {name}"))
-        .child(
-            button_group()
-                .child(
-                    button()
-                        .class([bs::BTN, bs::BTN_SECONDARY, bs::DROPDOWN_TOGGLE])
-                        .id("TODO")
-                        .attribute("data-bs-toggle", "dropdown")
-                        .r#type("button")
-                        .aria_expanded("false")
-                        .text(name),
-                )
-                .child(
-                    ul().class([bs::DROPDOWN_MENU])
-                        .aria_labelledby("TODO")
-                        .children([dropdown_item("Run"), dropdown_item("Pause")]),
-                ),
-        )
+        .child(dropdown(name))
         .child(
             button()
                 .r#type("button")
@@ -64,21 +67,19 @@ fn function(name: &str, icon: &str, is_last: bool) -> Element {
                 .child(i().class([icon])),
         );
 
-    if is_last {
-        column([bs::ALIGN_ITEMS_START]).child(function)
-    } else {
-        column([bs::ALIGN_ITEMS_STRETCH]).child(
-            row([bs::ALIGN_ITEMS_CENTER])
-                .child(function)
-                .child(horizontal_line())
-                .child(arrow_right()),
-        )
-    }
-    .into()
+    row([bs::ALIGN_ITEMS_CENTER])
+        .child(function)
+        .child(horizontal_line())
+        .child(arrow_right())
+        .into()
 }
 
-fn collapsed_function(name: &str, is_last: bool) -> Element {
-    function(name, "bi-zoom-in", is_last)
+fn end() -> Element {
+    dropdown("end")
+}
+
+fn collapsed_function(name: &str) -> Element {
+    function(name, "bi-zoom-in")
 }
 
 fn horizontal_line() -> Element {
@@ -89,13 +90,10 @@ fn arrow_right() -> Element {
     div().class([css::ARROW]).into()
 }
 
-fn expanded_function(
-    name: &str,
-    body: impl IntoIterator<Item = Element>,
-    is_last: bool,
-) -> Element {
+fn expanded_function(name: &str, body: impl IntoIterator<Item = Element>) -> Element {
     let body = row([
         css::SPEECH_BUBBLE_TOP,
+        bs::ALIGN_ITEMS_START,
         bs::SHADOW,
         bs::MT_3,
         bs::P_3,
@@ -103,8 +101,9 @@ fn expanded_function(
         bs::BORDER_SECONDARY,
         bs::ROUNDED,
     ])
-    .children(body);
-    let main = function(name, "bi-zoom-out", is_last);
+    .children(body)
+    .child(end());
+    let main = function(name, "bi-zoom-out");
 
     column([bs::ALIGN_ITEMS_STRETCH])
         .child(main)
@@ -115,21 +114,22 @@ fn expanded_function(
 fn main() {
     mount(
         "app",
-        column([bs::M_3, bs::ALIGN_ITEMS_START, bs::OVERFLOW_AUTO]).child(expanded_function(
-            "main_function",
-            [
-                collapsed_function("function1", false),
-                expanded_function(
-                    "another_function",
-                    [
-                        collapsed_function("child_function1", false),
-                        collapsed_function("child_function2", true),
-                    ],
-                    false,
-                ),
-                collapsed_function("function2", true),
-            ],
-            true,
-        )),
+        row([bs::M_3, bs::ALIGN_ITEMS_START, bs::OVERFLOW_AUTO]).children([
+            expanded_function(
+                "main_function",
+                [
+                    collapsed_function("function1"),
+                    expanded_function(
+                        "another_function",
+                        [
+                            collapsed_function("child_function1"),
+                            collapsed_function("child_function2"),
+                        ],
+                    ),
+                    collapsed_function("function2"),
+                ],
+            ),
+            end(),
+        ]),
     );
 }
